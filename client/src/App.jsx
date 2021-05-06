5c214f7be6fc7c7bad1467ed6d1baf992d0c6a20
janson_cheung <jansoncheung0004@gmail.com>
2021-05-06 20:49:37 +0800

    feat: added App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,12 @@
 import { ThemeProvider } from "styled-components/macro";
 import { COLOR, GlobalStyle, ResetStyle } from "./components/GlobalStyle";
+import { Switch, Route } from "react-router-dom";
+import styled from "styled-components/macro";
+import { AuthContext } from "./contexts/AuthContext";
+import axios from "axios";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import Login from "./views/Login";
 
 function App() {
   return (
@@ -8,6 +15,11 @@ function App() {
         <GlobalStyle />
         <ResetStyle />
       </ThemeProvider>
+      <Switch>
+        <Route exact path="/">
+          <Login />
+        </Route>
+      </Switch>
     </>
   );
 }

ecbd896378e016e3d951c71fd862543e90f9492a
Tomas2050 <adcapc@gmail.com>
2021-05-06 16:47:10 +0800

    fix: api key
    use dotenv for hiding api key

--- server/populartimes_api.py
+++ server/populartimes_api.py
@@ -1,5 +1,12 @@
 import sys
 import json
+import os
+
+from populartimes import populartimes
+from dotenv import load_dotenv
+load_dotenv()
+
+API_KEY = os.getenv('API_KEY')
 
 from populartimes import populartimes
 

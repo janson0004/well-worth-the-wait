fe3e09644ad3b5fd829c089432d2cee33ce5ac71
IamMrandrew <andrewliyanlap000824@gmail.com>
2021-05-07 18:13:12 +0800

    feat: dynamic chart

--- client/src/services/RestaurantsService.jsx
+++ client/src/services/RestaurantsService.jsx
@@ -4,6 +4,9 @@ const RestaurantService = {
   getAll: () => {
     return axios.get("/restaurant", { withCredentials: true });
   },
+  getTime: (placeId) => {
+    return axios.get(`/restaurant/time/${placeId}`, { withCredentials: true });
+  },
 };
 
 export default RestaurantService;
--- client/src/views/Place.jsx
+++ client/src/views/Place.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState, useContext } from "react";
+import { useParams } from "react-router-dom";
 import styled from "styled-components/macro";
 import {
   GoogleMap,
@@ -8,14 +9,15 @@ import {
 } from "@react-google-maps/api";
 import Container from "@material-ui/core/Container";
 import { FaThumbtack, FaStar } from "react-icons/fa";
-import Heart from "react-animated-heart";
+import Heart from "../components/ReactAnimatedHeart";
+import Skeleton from "react-loading-skeleton";
 import Chart from "../components/Chart";
 import { MEDIA_BREAK } from "../components/GlobalStyle";
 import Comment from "../components/Comment";
 import TextField from "@material-ui/core/TextField";
 import { IoMdSend } from "react-icons/io";
 import { RestaurantsContext } from "../contexts/RestaurantsContext";
-import { useParams } from "react-router-dom";
+import RestaurantService from "../services/RestaurantsService";
 
 const mapContainerStyle = {
   width: "100%",
@@ -28,58 +30,75 @@ const options = {
   zoomControl: true,
 };
 
-const waitTimeData = {
-  ten_hour_wait: [0, 0, 0, 0, 0, 15, 15, 0, 0, 0],
-  seven_day_wait: [0, 0, 0, 0, 0, 0, 0],
-};
-
-const waitTimeLabel = {
-  ten_hour_wait: [
-    "11:00",
-    "12:00",
-    "13:00",
-    "14:00",
-    "15:00",
-    "16:00",
-    "17:00",
-    "18:00",
-    "19:00",
-    "20:00",
-  ],
-  seven_day_wait: ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"],
-};
-
-// const restaurant = {
-//   placeId: "ChIJc91gg_IDBDQRTSK8--GRgCI",
-//   name: "Fishing Heya",
-//   address:
-//     "Shop B3, Treasure World, Site 11, Whampoa Garden, Hung Hom, Hong Kong",
-//   rating: 3.9,
-//   latitude: 22.3061193,
-//   longitude: 114.260494,
-// };
-
 const Place = () => {
   const [libraries] = useState(["places"]);
-  const [center, setCenter] = useState({
-    lat: 22.3061193,
-    lng: 114.260494,
-  });
   const { isLoaded, loadError } = useLoadScript({
     googleMapsApiKey: process.env.REACT_APP_MAP_API_KEY,
     libraries,
   });
 
   const [showInfoWindow, setshowInfoWindow] = useState(false);
+  const [loading, setLoading] = useState(true);
   const { restaurants, setRestaurants } = useContext(RestaurantsContext);
   const [restaurant, setRestaurant] = useState(null);
+  const [waitTime, setWaitTime] = useState(null);
+  const [waitTimeLabel, setWaitTimeLabel] = useState({
+    ten_hour_wait: [
+      "11:00",
+      "12:00",
+      "13:00",
+      "14:00",
+      "15:00",
+      "16:00",
+      "17:00",
+      "18:00",
+      "19:00",
+      "20:00",
+    ],
+    seven_day_wait: ["SUN", "MON", "TUE", "WED", "THU", "FRI", "SAT"],
+  });
 
   const [fav, setFav] = useState(false);
   const { id } = useParams();
 
   useEffect(() => {
+    const zeroPad = (num, size) => {
+      num = num.toString();
+      while (num.length < size) {
+        num = "0" + num;
+      }
+      return num;
+    };
+
+    const timeRange = (num) => {
+      if (num < 0) {
+        num += 24;
+      }
+      return num;
+    };
+
+    let date = new Date();
+    let hour = date.getHours();
+    let labels = [];
+    for (let i = hour - 9; i <= hour; i++) {
+      labels.push(`${zeroPad(timeRange(i), 2)}:00`);
+    }
+    setWaitTimeLabel({ ...waitTimeLabel, ten_hour_wait: labels });
+  }, []);
+  useEffect(() => {
     setRestaurant(restaurants.find((restaurant) => restaurant.placeId === id));
   }, [restaurants, id]);
+
+  useEffect(() => {
+    RestaurantService.getTime(id)
+      .then((res) => {
+        setWaitTime(res.data);
+        setLoading(false);
+      })
+      .catch((error) => {
+        setLoading(false);
+      });
+  }, [restaurant, id]);
 
   const infoWindowHandler = () => {
     setshowInfoWindow(!showInfoWindow);
@@ -93,7 +112,7 @@ const Place = () => {
         <GoogleMap
           mapContainerStyle={mapContainerStyle}
           zoom={20}
-          center={center}
+          center={{ lat: restaurant.latitude, lng: restaurant.longitude }}
           options={options}
         >
           {/* Marker props
@@ -103,9 +122,16 @@ const Place = () => {
           origin: new window.google.maps.Point(0, 0), 
           anchor: new window.google.map.Point(15, 15)
         }} */}
-          <Marker position={center} onClick={infoWindowHandler} />
+          <Marker
+            position={{ lat: restaurant.latitude, lng: restaurant.longitude }}
+            onClick={infoWindowHandler}
+          />
           {showInfoWindow && (
-            <InfoWindow position={center} onCloseClick={infoWindowHandler}>
+            <InfoWindow
+              position={{ lat: restaurant.latitude, lng: restaurant.longitude }}
+              onCloseClick={infoWindowHandler}
+              options={{ pixelOffset: new window.google.maps.Size(0, -50) }}
+            >
               <>
                 <InfoWindowName>{restaurant.name}</InfoWindowName>
                 <InfoWindowAddress>{restaurant.address}</InfoWindowAddress>
@@ -119,32 +145,50 @@ const Place = () => {
             <Info>
               <NameWrapper>
                 <Name>{restaurant.name}</Name>
-                <Heart isClick={fav} onClick={() => setFav(!fav)} />
+                <Heart clicked={fav ? 1 : 0} onClick={() => setFav(!fav)} />
               </NameWrapper>
               <Address>{restaurant.address}</Address>
               <RatingWrapper>
                 <Rating>{restaurant.rating}</Rating>
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
-                <FaStar />
+                {[...Array(Math.round(restaurant.rating))].map(
+                  (star, index) => (
+                    <CustomFaStar key={index} active={true ? 1 : 0} />
+                  )
+                )}
+
+                {[...Array(5 - Math.round(restaurant.rating))].map(
+                  (star, index) => (
+                    <CustomFaStar key={index} active={false ? 1 : 0} />
+                  )
+                )}
               </RatingWrapper>
               <LocationWrapper>
                 <FaThumbtack />
                 <Location>{`${restaurant.latitude}, ${restaurant.longitude}`}</Location>
               </LocationWrapper>
             </Info>
-            <Chart
-              title="Waiting Time in the past 10 hours"
-              data={waitTimeData.ten_hour_wait}
-              labels={waitTimeLabel.ten_hour_wait}
-            />
-            <Chart
-              title="Waiting Time in this hour of past 7 days"
-              data={waitTimeData.seven_day_wait}
-              labels={waitTimeLabel.seven_day_wait}
-            />
+            {!loading && (
+              <>
+                <Chart
+                  title="Waiting Time in the past 10 hours"
+                  data={waitTime.ten_hour_wait}
+                  labels={waitTimeLabel.ten_hour_wait}
+                />
+                <Chart
+                  title="Waiting Time in this hour of past 7 days"
+                  data={waitTime.seven_day_wait}
+                  labels={waitTimeLabel.seven_day_wait}
+                />
+              </>
+            )}
+            {loading && (
+              <>
+                <CustomSkeleton height={40} />
+                <CustomSkeleton height={400} />
+                <CustomSkeleton height={40} />
+                <CustomSkeleton height={400} />
+              </>
+            )}
           </LeftWrapper>
           <RightWrapper>
             <Comments>
@@ -244,12 +288,6 @@ const RatingWrapper = styled.div`
   display: flex;
   align-items: top;
   margin-bottom: 12px;
-
-  svg {
-    font-size: 18px;
-    color: #ffb800;
-    margin-right: 8px;
-  }
 `;
 
 const Rating = styled.span`
@@ -257,6 +295,12 @@ const Rating = styled.span`
   font-weight: 500;
   display: block;
   margin-right: 15px;
+`;
+
+const CustomFaStar = styled(FaStar)`
+  font-size: 18px;
+  color: ${(props) => (props.active ? "#ffb800" : "#aab8c2")};
+  margin-right: 8px;
 `;
 
 const LocationWrapper = styled.div`
@@ -310,4 +354,8 @@ const CommentsTitle = styled.span`
   font-size: 30px;
   font-weight: 700;
   margin-bottom: 12px;
+`;
+
+const CustomSkeleton = styled(Skeleton)`
+  margin-bottom: 20px;
 `;
